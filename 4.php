3c5b87eda1e491b895aeebaa3d9d65af7eeec6f1
sun-way <37132870+sun-way@users.noreply.github.com>
2018-07-08 15:09:50 +0300

    Update 4.php

--- 4.php
+++ 4.php
@@ -1,7 +1,6 @@
 <?php
 
 // формируем урл для запроса для Москвы
-
 $url='http://api.openweathermap.org/data/2.5/find?q=Moscow,RU&type=like&APPID=3a41967d489d2595e96206e848c4f0c4';
 
 $data = file_get_contents($url);
@@ -29,8 +28,6 @@ if($data){
         $temp_mi=round($temp_mi,0);
         echo "миним темп-ра: " .$temp_mi." °C" ."<br/>";
 
-        //echo "temp_min: " . $oneDay->main->temp_min-273 . "<br/>";
-        //echo "Скорость ветра: " . $oneDay->main->speed . "<br/>";
         echo "Погода: " . $oneDay->weather[0]->description . "<br/>";
 
         $press=$oneDay->main->pressure;
@@ -38,13 +35,11 @@ if($data){
         $press=round($press,1);
         echo "Давление: " . $press . "  мм.рт.ст". "<br/>";
         echo "Влажность: " . $oneDay->main->humidity . "<br/>";
-        //echo "Текущая  температура : " . $oneDay ->main->temp . "<br/>";
 
         $temp3=$oneDay->main->temp;
         $temp_ob=$temp3-273;
         $temp_ob=round($temp_ob,0);
         echo "Текущая  температура: " .$temp_ob." °C" ."<br/>";
-        //  weather -> day -> today -> temperature;
         echo "<hr/>";
     }
 }else{
